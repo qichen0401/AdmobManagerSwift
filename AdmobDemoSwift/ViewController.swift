e319af0ce2af8c68357ebfd6d1cb9f62ef5a5094
Qi Chen <qichen0401@gmail.com>
2018-03-08 16:24:26 +0800

    Initial Commit

---
+++ AdmobDemoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdmobDemoSwift
+//
+//  Created by Qi Chen on 08/03/2018.
+//  Copyright © 2018 Qi Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
